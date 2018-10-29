64233f4469c47f42b4813c42e8f6be6c271c1e22
Michal Srb <michal@redhat.com>
2018-10-29 14:49:48 +0100

    Migrate tests to victimsdb-lib

--- .qa/tests/test_validate.py
+++ .qa/tests/test_validate.py
@@ -1,50 +1,35 @@
 import os
-import yaml
+from victimsdb_lib.model import Record
 
 
 def iter_records():
-    for yaml_path in iter_yaml_paths():
+    for yaml_path, ecosystem in iter_yaml_paths():
+
         with open(yaml_path, 'r') as f:
-            yaml_dict = yaml.safe_load(f)
-            yield yaml_path, yaml_dict
+            record = Record.from_file(yaml_path, ecosystem=ecosystem)
+            yield yaml_path, record, ecosystem
 
 
 def iter_yaml_paths():
     for dirpath, dirnames, filenames in os.walk('../database/', followlinks=False):
         for filename in filenames:
             if filename.endswith('.yaml'):
+                ecosystem = os.path.split(os.path.split(dirpath)[0])[1]
                 yaml_path = os.path.join(dirpath, filename)
-                yield yaml_path
-
-
-def test_parse():
-    """Test that all YAML files can be parsed."""
-    for yaml_path in iter_yaml_paths():
-        with open(yaml_path, 'r') as f:
-            try:
-                yaml.safe_load(f)
-            except yaml.YAMLError as exc:
-                assert False, 'Unable to parse {f} {e}'.format(f=yaml_path, e=str(exc))
+                yield yaml_path, ecosystem
 
 
 def test_has_required():
-    """Test that all required fields are present.
+    """Test that all required fields are present."""
 
-    TODO: we could use JSON schema to validate the YAML files.
-    """
-    for path, record in iter_records():
-        assert record.get('cve'), 'missing "cve" field in {f}'.format(f=path)
-        assert record.get('title'), 'missing "title" field in {f}'.format(f=path)
-        assert record.get('description'), 'missing "description" field in {f}'.format(f=path)
-        assert record.get('cvss_v2'), 'missing "cvss_v2" field in {f}'.format(f=path)
-        assert record.get('references'), 'missing "references" field in {f}'.format(f=path)
-        assert record.get('affected'), 'missing "affected" field in {f}'.format(f=path)
+    for path, record, ecosystem in iter_records():
+        assert record.cve_id, 'missing "cve" field in {f}'.format(f=path)
+        assert record.title, 'missing "title" field in {f}'.format(f=path)
+        assert record.description, 'missing "description" field in {f}'.format(f=path)
+        assert record.cvss_v2, 'missing "cvss_v2" field in {f}'.format(f=path)
+        assert record.references, 'missing "references" field in {f}'.format(f=path)
+        assert record.affected, 'missing "affected" field in {f}'.format(f=path)
 
-        for affected in record.get('affected'):
-            assert affected.get('version'), 'missing "affected.version" field in {f}'.format(f=path)
-
-            if '/java/' in path:
-                assert affected.get('groupId'), 'missing "affected.groupId" field in {f}'.format(f=path)
-                assert affected.get('artifactId'), 'missing "affected.artifactId" field in {f}'.format(f=path)
-            else:
-                assert affected.get('name'), 'missing "affected.name" field in {f}'.format(f=path)
+        for affected in record.affected:
+            assert affected.version, 'missing "affected.version" field in {f}'.format(f=path)
+            assert affected.name, 'missing "affected.name" field in {f}'.format(f=path)
